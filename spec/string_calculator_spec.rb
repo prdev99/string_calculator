60437bc68e6e067f13a806409f5cbd3482c98a87
Prakash Namdev <namdevprakash99@gmail.com>
2024-05-28 18:00:37 +0530

    Implementation to handle all possible inputs and update rspec

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -8,5 +8,33 @@ RSpec.describe StringCalculator do
     it 'returns 0 for an empty string' do
       expect(StringCalculator.add('')).to eq(0)
     end
+
+    it 'returns the number for a single number' do
+      expect(StringCalculator.add('1')).to eq(1)
+    end
+
+    it 'returns the sum of two numbers' do
+      expect(StringCalculator.add('1,2')).to eq(3)
+    end
+
+    it 'returns the sum of multiple numbers' do
+      expect(StringCalculator.add('1,2,3,4')).to eq(10)
+    end
+
+    it 'handles new lines between numbers' do
+      expect(StringCalculator.add("1\n2,3")).to eq(6)
+    end
+
+    it 'supports different delimiters' do
+      expect(StringCalculator.add("//;\n1;2")).to eq(3)
+    end
+
+    it 'raises an exception for negative numbers' do
+      expect { StringCalculator.add('1,-2') }.to raise_error('negative numbers not allowed: -2')
+    end
+
+    it 'raises an exception for multiple negative numbers' do
+      expect { StringCalculator.add('1,-2,-3') }.to raise_error('negative numbers not allowed: -2, -3')
+    end
   end
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -1,6 +1,18 @@
-# string_calculator.rb
 class StringCalculator
-  def self.add(_input)
-    0
+  def self.add(input)
+    return 0 if input.empty?
+
+    if input.start_with?('//')
+      delimiter = input[2]
+      input = input[4..-1]
+    else
+      delimiter = /,|\n/
+    end
+
+    numbers = input.split(delimiter).map(&:to_i)
+    negatives = numbers.select { |n| n < 0 }
+    raise "negative numbers not allowed: #{negatives.join(', ')}" unless negatives.empty?
+
+    numbers.sum
   end
 end
